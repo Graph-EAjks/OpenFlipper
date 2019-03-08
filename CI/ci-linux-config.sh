d9d71ee4042d8f5d49998a58c3f620bd56ba26c1
Jan Möbius <moebius@cs.rwth-aachen.de>
2019-03-08 07:32:16 +0100

    Run one job with disabled python system

--- CI/ci-linux-config.sh
+++ CI/ci-linux-config.sh
@@ -56,3 +56,7 @@ elif [ "$QTVERSION" == "QT5.9.0" ]; then
   BUILDPATH="$BUILDPATH-qt5.9.0"
   OPTIONS="$OPTIONS -DFORCE_QT4=FALSE -DQWT6_INCLUDE_DIR=~/sw/qwt-6.1.3-qt5.9.0/include -DQWT6_LIBRARY_DIR=~/sw/qwt-6.1.3-qt5.9.0/lib -DQWT6_LIBRARY=~/sw/qwt-6.1.3-qt5.9.0/lib/libqwt.so -DQT5_INSTALL_PATH=~/sw/Qt/5.9/gcc_64"
 fi
+
+if [ "$PYTHON" == "no" ]; then
+  OPTIONS="$OPTIONS -DDISABLE_OPENFLIPPER_PYTHON_SYSTEM=TRUE"
+fi
