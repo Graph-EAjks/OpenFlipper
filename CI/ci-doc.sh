d37e4691334720da65e9dd3be46da94941f55eb4
Jan Möbius <moebius@cs.rwth-aachen.de>
2019-09-25 09:44:56 +0200

    Fixed user for upload of doc

--- CI/ci-doc.sh
+++ CI/ci-doc.sh
@@ -14,5 +14,5 @@ cmake -DCMAKE_BUILD_TYPE=Release -DOPENFLIPPER_BUILD_UNIT_TESTS=FALSE ../
 make doc >> Doxygen-warning.log 2> Doxygen-error.log
 
 # Copy to webserver
-scp -r -P 2222 Build/share/OpenFlipper/Doc/DeveloperHTML/* hudson@web4-info8:OpenFlipper-Free-Daily/Doc/Free/Developer
-scp -r -P 2222 Build/share/OpenFlipper/Doc/UserHTML/* hudson@web4-info8:OpenFlipper-Free-Daily/Doc/Free/User
+scp -r -P 2222 Build/share/OpenFlipper/Doc/DeveloperHTML/* gitlab@web4-info8:OpenFlipper-Free-Daily/Doc/Free/Developer
+scp -r -P 2222 Build/share/OpenFlipper/Doc/UserHTML/* gitlab@web4-info8:OpenFlipper-Free-Daily/Doc/Free/User
