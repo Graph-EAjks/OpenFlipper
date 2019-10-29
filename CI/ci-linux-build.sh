00617fbfddd89e4326583d74812dc5b18744983a
Johannes Lenzen <johannes.lenzen@rwth-aachen.de>
2019-10-29 15:41:25 +0100

    Changed clone url

--- CI/ci-linux-build.sh
+++ CI/ci-linux-build.sh
@@ -22,7 +22,7 @@ echo "Full cmake options: $OPTIONS  "
 ########################################
 rm -rf TestData
 #git clone git@roosevelt:moebius/OpenFlipper-Test-Data.git TestData
-git clone https://roosevelt:9000/moebius/OpenFlipper-Test-Data.git TestData
+git clone https://graphics.rwth-aachen.de:9000/moebius/OpenFlipper-Test-Data.git TestData
 
 
 #########################################
