dc263c99a2118782d4840ea5c9acb0ffef99a745
Johannes Lenzen <johannes.lenzen@rwth-aachen.de>
2019-11-05 13:24:52 +0100

    Testing

--- CI/ci-linux-config.sh
+++ CI/ci-linux-config.sh
@@ -21,7 +21,7 @@ if [ "$COMPILER" == "gcc" ]; then
   BUILDPATH="$BUILDPATH-gcc"
 
   # without icecc: no options required
-  OPTIONS="$OPTIONS -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_C_COMPILER=/usr/bin/gcc -DQT_NO_VERSION_TAGGING"
+  OPTIONS="$OPTIONS -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_C_COMPILER=/usr/bin/gcc -DQT_NO_VERSION_TAGGING "
   MAKE_OPTIONS="-j16"
   export ICECC_CXX=/usr/bin/g++ ; export ICECC_CC=/usr/bin/gcc
 
