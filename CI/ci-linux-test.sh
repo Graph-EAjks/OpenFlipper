deb8551c90cbda9f71cb2688ffb9c6738157a05c
Johannes Lenzen <johannes.lenzen@rwth-aachen.de>
2019-11-12 11:37:19 +0100

    Readded testdata

--- CI/ci-linux-test.sh
+++ CI/ci-linux-test.sh
@@ -22,7 +22,7 @@ rm -rf artifacts
 ########################################
 rm -rf TestData
 #git clone git@roosevelt:moebius/OpenFlipper-Test-Data.git TestData
-#git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@www.graphics.rwth-aachen.de:9000/moebius/OpenFlipper-Test-Data.git TestData
+git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@www.graphics.rwth-aachen.de:9000/moebius/OpenFlipper-Test-Data.git TestData
 
 #########################################
 # Run Release Unittests
