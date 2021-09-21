d7bf4b38995d81ad0032043e8a181dbb656a0184
Jan Möbius <moebius@cs.rwth-aachen.de>
2021-09-21 10:50:55 +0200

    Fixed variable rename

--- CI/ci-mac-deploy.sh
+++ CI/ci-mac-deploy.sh
@@ -27,7 +27,7 @@ fi
 
 # Build without ports to avoid qt4 collision
 # fix pathes if run on a different machine
-/opt/local/bin/cmake ../ -DCMAKE_CXX_FLAGS='-std=c++11' -DQWT6_INCLUDE_DIR=/Users/jenkins/sw/qwt-6.1.3-qt5.11.3/lib/qwt.framework/Headers -DQWT6_LIBRARY=/Users/jenkins/sw/qwt-6.1.3-qt5.11.3/lib/qwt.framework -DQT5_INSTALL_PATH=/Users/jenkins/sw/Qt/5.11.3/clang_64/   
+/opt/local/bin/cmake ../ -DCMAKE_CXX_FLAGS='-std=c++11' -DQWT6_INCLUDE_DIR=/Users/jenkins/sw/qwt-6.1.3-qt5.11.3/lib/qwt.framework/Headers -DQWT6_LIBRARY=/Users/jenkins/sw/qwt-6.1.3-qt5.11.3/lib/qwt.framework -DQT_INSTALL_PATH=/Users/jenkins/sw/Qt/5.11.3/clang_64/   
 
 rm -f *.dmg 
 
