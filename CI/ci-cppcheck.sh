aad1a4e564a0861a093d14c8f2c21b2a8dbef2e8
Jan Möbius <moebius@cs.rwth-aachen.de>
2023-01-31 14:27:17 +0100

    Reduce allowed cppcheck errors

--- CI/ci-cppcheck.sh
+++ CI/ci-cppcheck.sh
@@ -60,7 +60,7 @@ echo "CPPCHECK Summary"
 echo "=============================================================================="
 echo -e "${NC}"
 
-MAX_ERROR=1793
+MAX_ERROR=1605
 
 if [ $COUNT -gt $MAX_ERROR ]; then
   echo -e ${WARNING}
